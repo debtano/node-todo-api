270f41831df160e6ddb2d2f96230f419c33640c1
hantoli <hantoli@amazon.com>
2021-11-30 15:24:54 -0300

    Add queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,38 @@
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+const {ObjectID} = require('mongodb');
+
+let userId = '619fa6efd8bbeeb84ac6f1d6';
+let id = '61a5453ce70c7ba81906d497';
+
+if (!ObjectID.isValid(id)) {
+  console.log('ID not valid');
+}
+
+// Todo.find({
+//   _id : id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+Todo.findById(id).then((todo) => {
+  if (!todo) {
+    return console.log('Id not found');
+  }
+  console.log('Todo by Id', todo);
+}).catch((e) => console.log(e))
+
+User.findById(userId).then((user) => {
+  if (!user) {
+    return console.log('Unable to find user');
+  }
+  console.log(JSON.stringify(user, undefined, 2));
+}).catch((e) => console.log(e))
